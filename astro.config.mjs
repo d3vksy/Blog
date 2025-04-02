2c7992ddb80c769221fc12958ebbbaea37daa4c6
wolpis <star@devksy.xyz>
2025-04-02 20:45:54 +0900

    배포 테스트

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,7 +19,7 @@ import { parseDirectiveNode } from "./src/plugins/remark-directive-rehype.js";
 import { remarkExcerpt } from "./src/plugins/remark-excerpt.js";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
-// https://astro.build/config
+// https://astro.build/config 
 export default defineConfig({
   site: 'https://d3vksy.github.io',
   trailingSlash: "always",
